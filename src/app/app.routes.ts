1b67ac7a2647fbf29be331b982ac6ed9fe54ecbb
iparit <ipticona019@gmail.com>
2024-06-12 01:31:09 -0400

    subiendo al repositorio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'menu', component:MenuComponent
+    },
+    {
+        path:'body', component:BodyComponent
+    },
+    {
+        path:'footer', component:FooterComponent
+    }
+    
+];
